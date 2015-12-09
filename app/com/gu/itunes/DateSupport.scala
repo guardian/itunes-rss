d01a6971e27f6d8bc9e4fa1765fb54e6e9414160
oilnam <manlio.poltronieri@theguardian.com>
2015-12-09 12:19:58 +0000

    Better variable naming

--- app/com/gu/itunes/DateSupport.scala
+++ app/com/gu/itunes/DateSupport.scala
@@ -6,8 +6,8 @@ import org.joda.time.format.DateTimeFormat
 object DateSupport {
   val dateFormatter = DateTimeFormat.forPattern("EEE, dd MMM yyyy HH:mm:ss")
 
-  def toRssTimeFormat(someTime: DateTime) = {
-    someTime.withZone(DateTimeZone.UTC).toString(dateFormatter) + " GMT"
+  def toRssTimeFormat(dateTime: DateTime) = {
+    dateTime.withZone(DateTimeZone.UTC).toString(dateFormatter) + " GMT"
   }
 }
 
