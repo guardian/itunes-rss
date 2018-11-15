59192b193906bbfdb7f427df7f53aff324d8ada9
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-11-15 10:32:51 +0000

    Remove RssItemStrategy

--- app/com/gu/itunes/RssItemStrategy.scala
+++
@@ -1,9 +0,0 @@
-package com.gu.itunes
-
-import com.gu.contentapi.client.model.v1.Content
-import java.time.Instant
-
-trait ItemStrategy {
-  def cta(standfirst: String): String
-  def guid(content: Content): String
-}
