cb37490d99eae426e7f25d181a44903d95e359b3
Juan Vaccari <juan.vaccari@guardian.co.uk>
2020-11-26 15:07:11 +0000

    Remove trailing comma

--- app/com/gu/itunes/iTunesRssItem.scala
+++ app/com/gu/itunes/iTunesRssItem.scala
@@ -125,7 +125,7 @@ class iTunesRssItem(val podcast: Content, val tagId: String, asset: Asset) {
         AcastLaunchGroup(new DateTime(2020, 6, 18, 0, 0), Seq(
           "society/series/innermost")),
         AcastLaunchGroup(new DateTime(2020, 11, 26, 0, 0), Seq(
-          "australia-news/series/temporary")),
+          "australia-news/series/temporary"))
       )
 
       val useAcastProxy: Boolean = acastPodcasts.find(_.tagIds.contains(tagId)).exists(p => lastModified.isAfter(p.launchDate))
