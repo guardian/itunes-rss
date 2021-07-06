5bb59b067dad38901865e8d0a89a1dde3ab49179
Tony McCrae <tony.mccrae.freelancer@guardian.co.uk>
2021-07-06 16:28:59 +0100

    Populate the gguid isPermaLink field which is required by some 3rd parties.
    "guid must be a full URL, unless isPermaLink attribute is false" (https://validator.w3.org/feed/docs/error/InvalidPermalink.html)
    
    In late 2018 we started using internal ids for the guid. These are not full urls so we set to false after this date.
    
    One of the items in the test file has had it's date shifted to exercise this case.

--- app/com/gu/itunes/iTunesRssItem.scala
+++ app/com/gu/itunes/iTunesRssItem.scala
@@ -129,8 +129,7 @@ class iTunesRssItem(val podcast: Content, val tagId: String, asset: Asset) {
         AcastLaunchGroup(new DateTime(2021, 1, 19, 0, 0), Seq(
           "music/series/reverberate")),
         AcastLaunchGroup(new DateTime(2021, 6, 8, 0, 0), Seq(
-          "lifeandstyle/series/comforteatingwithgracedent"))
-      )
+          "lifeandstyle/series/comforteatingwithgracedent")))
 
       val useAcastProxy: Boolean = acastPodcasts.find(_.tagIds.contains(tagId)).exists(p => lastModified.isAfter(p.launchDate))
       if (useAcastProxy) "https://flex.acast.com/" + url.replace("https://", "") else url
@@ -152,11 +151,13 @@ class iTunesRssItem(val podcast: Content, val tagId: String, asset: Asset) {
     val capiUrl = asset.file.getOrElse("")
     val regex = s"""https?://static(-secure)?.guim.co.uk/audio/kip/$tagId"""
     val guid = {
-      val launchDay = new DateTime(2018, 11, 14, 0, 0)
-      val default = capiUrl.replaceAll(regex, "http://download.guardian.co.uk/draft/audio")
-      if (lastModified.isAfter(launchDay))
-        podcast.fields.flatMap(_.internalComposerCode).getOrElse(default)
-      else
+      val composerBasedGuidsLaunchDate = new DateTime(2018, 11, 14, 0, 0)
+      val default = (capiUrl.replaceAll(regex, "http://download.guardian.co.uk/draft/audio"), true)
+      if (lastModified.isAfter(composerBasedGuidsLaunchDate)) {
+        // We use the internal composer code as the guid from this time on
+        // These are not perma links.
+        podcast.fields.flatMap(_.internalComposerCode.map((_, false))).getOrElse(default)
+      } else
         default
     }
 
@@ -183,7 +184,7 @@ class iTunesRssItem(val podcast: Content, val tagId: String, asset: Asset) {
       <description> { description } </description>
       <enclosure url={ url } length={ sizeInBytes } type={ mType }/>
       <pubDate>{ pubDate }</pubDate>
-      <guid>{ guid }</guid>
+      <guid isPermaLink={ guid._2.toString }>{ guid._1 }</guid>
       <itunes:duration>{ duration }</itunes:duration>
       <itunes:author>{ iTunesRssFeed.author }</itunes:author>
       {
