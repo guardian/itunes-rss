ea6f1e920418941492f5048096e26a8d180f4722
Juan Vaccari <juan.vaccari@guardian.co.uk>
2021-12-23 16:50:22 +0000

    Remove commented code

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -63,10 +63,6 @@ class Application(val controllerComponents: ControllerComponents, val config: Co
       .showFields("webTitle,webPublicationDate,standfirst,trailText,internalComposerCode")
 
     def fetchItemsWithPagination(query: ItemQuery, page: Int = 1, resps: Seq[ItemResponse] = Seq.empty): Future[Seq[ItemResponse]] = {
-      // The last page fetch may need to be smaller to prevent returning too many results
-      //val currentDepth = (page - 1) * pageSize
-      //val pageSizeForThisCall = Seq(maxItems - currentDepth, pageSize).min
-
       Logger.debug("Fetching page: " + page + " with page size: " + pageSize)
       val withPagination = query.page(page).pageSize(pageSize)
 
