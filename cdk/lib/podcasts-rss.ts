4afdd22102628c31ef501f77f25ab65b66615ef7
Andy Gallagher <fredex42@gmail.com>
2023-04-17 18:07:21 +0100

    assembled CDK mirroring existing CFN

--- cdk/lib/podcasts-rss.ts
+++ cdk/lib/podcasts-rss.ts
@@ -1,11 +1,91 @@
-import { join } from "path";
-import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import { GuStack } from "@guardian/cdk/lib/constructs/core";
-import type { App } from "aws-cdk-lib";
-import { CfnInclude } from "aws-cdk-lib/cloudformation-include";
+import type {GuStackProps} from "@guardian/cdk/lib/constructs/core";
+import {GuParameter, GuStack} from "@guardian/cdk/lib/constructs/core";
+import type {App} from "aws-cdk-lib";
+import {GuEc2App, GuPlayApp} from "@guardian/cdk";
+import {AccessScope} from "@guardian/cdk/lib/constants";
+import {InstanceClass, InstanceSize, InstanceType, Vpc} from "aws-cdk-lib/aws-ec2";
+import {policies} from "./policies";
+import {aws_ssm} from "aws-cdk-lib";
+import {GuVpc} from "@guardian/cdk/lib/constructs/ec2";
 
 export class PodcastsRss extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
+
+    const urgentAlarmTopicArn = aws_ssm.StringParameter.fromStringParameterName(this, "urgent-alarm-arn", "/account/content-api-common/alarms/urgent-alarm-topic");
+    const nonUrgentAlarmTopicArn = aws_ssm.StringParameter.fromStringParameterName(this, "non-urgent-alarm-arn", "/account/content-api-common/alarms/non-urgent-alarm-topic");
+
+    const vpcId = aws_ssm.StringParameter.valueForStringParameter(this, this.getVpcIdPath());
+    const vpc = Vpc.fromVpcAttributes(this, "vpc", {
+      vpcId: vpcId,
+      availabilityZones: ["eu-west-1a","eu-west-1b" ,"eu-west-1c"]
+    });
+
+    const subnetsList = new GuParameter(this, "subnets", {
+      description: "Subnets to deploy into",
+      default: this.getDeploymentSubnetsPath(),
+      fromSSM: true,
+      type: "List<String>"
+    });
+    const deploymentSubnets = GuVpc.subnets(this, subnetsList.valueAsList);
+
+    const hostedZone = aws_ssm.StringParameter.valueForStringParameter(this, `/account/services/content-aws.guardianapis/${this.stage}/hostedzoneid`);
+
+
+    new GuPlayApp(this, {
+      access: {
+        scope: AccessScope.PUBLIC,
+      },
+      app: "podcasts-rss",
+      applicationLogging: {
+        enabled: true,
+      },
+      certificateProps: {
+        domainName: this.stage==="CODE" ? "itunes-feed.content.code.dev-guardianapis.com" : "itunes-feed.content-aws.guardianapis.com",
+        hostedZoneId: hostedZone,
+      },
+      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.SMALL),
+      monitoringConfiguration: {
+        snsTopicName: urgentAlarmTopicArn.stringValue,
+        http5xxAlarm: {
+          tolerated5xxPercentage: 20,
+          numberOfMinutesAboveThresholdBeforeAlarm: 2,
+        },
+        unhealthyInstancesAlarm: true,
+      },
+      privateSubnets: deploymentSubnets,
+      publicSubnets: deploymentSubnets,
+      roleConfiguration: {
+        additionalPolicies: [policies(this)],
+      },
+      scaling: {
+        minimumInstances: 2,
+        maximumInstances: 4,
+      },
+      userData: {
+        distributable: {
+          fileName: "podcasts-rss.deb",
+          executionStatement: "dpkg -i podcasts-rss.deb"
+        }
+      },
+      vpc,
+    })
+  }
+
+  getAccountPath(elementName: string) {
+    const basePath = "/account/vpc";
+    if(this.stack.includes("preview")) {
+      return this.stage=="CODE" ? `${basePath}/CODE-preview/${elementName}` : `${basePath}/PROD-preview/${elementName}`;
+    } else {
+      return this.stage=="CODE" ? `${basePath}/CODE-live/${elementName}` : `${basePath}/PROD-live/${elementName}`;
+    }
+  }
+
+  getVpcIdPath() {
+    return this.getAccountPath("id");
+  }
+
+  getDeploymentSubnetsPath() {
+    return this.getAccountPath("subnets")
   }
 }
---
+++ cdk/lib/policies.ts
@@ -0,0 +1,35 @@
+import {Effect, PolicyStatement} from "aws-cdk-lib/aws-iam";
+import {GuStack} from "@guardian/cdk/lib/constructs/core";
+import {GuPolicy} from "@guardian/cdk/lib/constructs/iam";
+
+export const policies = (stack:GuStack) => new GuPolicy(stack, "Policies", {
+        statements: [
+            //FIXME - what is this needed for?
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ["cloudformation:DescribeStackResource"],
+                resources: ["*"]
+            }),
+            //FIXME - FAR TOO BROAD!!!!
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ["cloudwatch:*"],
+                resources: ["*"]
+            }),
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ["s3:GetObject"],
+                resources: [`arn:aws:s3:::content-api-config/podcasts-rss/${stack.stage}/*`]
+            }),
+            //FIXME = what is this needed for?
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: [
+                    "ec2:DescribeInstances",
+                    "autoscaling:DescribeAutoScalingGroups",
+                    "autoscaling:DescribeAutoScalingInstances"
+                ],
+                resources: ["*"]
+            })
+        ]
+    });
